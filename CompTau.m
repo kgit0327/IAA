6c7bd35dc1d144214c7ed847335f718050e8ae6e
kgit327 <kgit327@gmail.com>
2022-06-19 18:49:52 +0900

    add all

--- CompTau.m
+++ CompTau.m
@@ -1,4 +1,4 @@
-clear all
+% clear all
 
 load('NPMEAN.mat');
 
@@ -67,9 +67,8 @@ a_tor = tDeps.a_tor;
 
 Tau = nan(15, RM - GC + 1);
 
+parfor iFrame = 1 : RM - GC + 1
 
-parfor iFrame = 1 : RM - GC + 1
-    Tau_ = nan(15, 1);
     iFrame_ = GC - 1 + iFrame;
 
     th0_1 = th0(1, iFrame_);
@@ -127,7 +126,6 @@ Tau = nan(15, RM - GC + 1);
     th2d = [th2d0(:, iFrame_); th2d1(:, iFrame_); th2d2(:, iFrame_); th2d3(:, iFrame_); th2d4(:, iFrame_)];
 
     %%
-    tic
     GRA = find_GRA(g1,g2,g3,l0,l1,l2,l3,l4,m0,m1,m2,m3,m4,th0_1,th0_2,th0_3,th1_1,th1_2,th1_3,th2_1,th2_2,th2_3,th3_1,th3_2,th3_3,th4_1,th4_2,th4_3);
     INT = find_INT(I_01,I_02,I_03,I_11,I_12,I_13,I_21,I_22,I_23,I_31,I_32,I_33,I_41,I_42,I_43,a_tor_1,a_tor_2,a_tor_3,l0,l1,l2,l3,l4,m0,m1,m2,m3,m4,th0_1,th0_2,th0_3,th1_1,th1_2,th1_3,th2_1,th2_2,th2_3,th3_1,th3_2,th3_3,th4_1,th4_2,th4_3,thd0_1,thd0_2,thd0_3,thd1_1,thd1_2,thd1_3,thd2_1,thd2_2,thd2_3,thd3_1,thd3_2,thd3_3,thd4_1,thd4_2,thd4_3);
     Itheta = find_Itheta(I_01,I_02,I_03,I_11,I_12,I_13,I_21,I_22,I_23,I_31,I_32,I_33,I_41,I_42,I_43,l0,l1,l2,l3,l4,m0,m1,m2,m3,m4,th0_1,th0_2,th0_3,th1_1,th1_2,th1_3,th2_1,th2_2,th2_3,th3_1,th3_2,th3_3,th4_1,th4_2,th4_3);
